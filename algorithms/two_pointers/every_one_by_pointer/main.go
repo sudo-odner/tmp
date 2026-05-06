ac90631328cb45a63a39988ee55eff1fa9ea9ffa
sudo-odner <odner.main@gmail.com>
2026-05-06 20:30:44 +0300

    algorithms(evero_one_by_pointer): complete task

--- algorithms/two_pointers/every_one_by_pointer/main.go
+++ algorithms/two_pointers/every_one_by_pointer/main.go
@@ -48,10 +48,52 @@ func bothElements(arr1 []int, arr2 []int) []int {
 	return answer
 }
 
+// func main() {
+// 	arr1 := []int{0, 2, 4, 8, 8}
+// 	arr2 := []int{1, 2, 2, 7, 8, 8, 8}
+// 	arr3 := []int{1, 3, 4, 7}
+// 	arr4 := []int{0, 2, 5, 6}
+// 	fmt.Println(bothElements(arr1, arr2), bothElements(arr3, arr4))
+// }
+
+// Даны два отсортированных массива nums1 и nums2. Нужно объединить их в один отсортированный массив.
+//
+// Ввод:
+// nums1 = [1, 3, 5, 7], nums2 = [2, 4, 6, 8]
+// Вывод:
+// [1, 2, 3, 4, 5, 6, 7, 8]
+
+func inOneSortedArray(nums1, nums2 []int) []int {
+	p1 := 0
+	p2 := 0
+	answer := []int{}
+
+	for p1 < len(nums1) && p2 < len(nums2) {
+		if nums1[p1] == nums1[p2] {
+			answer = append(answer, nums1[p1], nums2[p2])
+			p1++
+			p2++
+		} else if nums1[p1] > nums2[p2] {
+			answer = append(answer, nums2[p2])
+			p2++
+		} else if nums1[p1] < nums2[p2] {
+			answer = append(answer, nums1[p1])
+			p1++
+		}
+	}
+	if len(nums1) > len(nums2) {
+		answer = append(answer, nums1[len(nums2):]...)
+	} else if len(nums1) < len(nums2) {
+		answer = append(answer, nums2[len(nums1):]...)
+	}
+	return answer
+}
+
 func main() {
-	arr1 := []int{0, 2, 4, 8, 8}
-	arr2 := []int{1, 2, 2, 7, 8, 8, 8}
-	arr3 := []int{1, 3, 4, 7}
-	arr4 := []int{0, 2, 5, 6}
-	fmt.Println(bothElements(arr1, arr2), bothElements(arr3, arr4))
+	nums1 := []int{1, 3, 5, 7}
+	nums2 := []int{2, 4, 6, 8}
+	// nums1 := []int{1, 3, 5, 7}
+	// nums2 := []int{2, 4, 6, 8, 9, 10, 11}
+
+	fmt.Println(inOneSortedArray(nums1, nums2))
 }
